eb75c98f0203df2a27c1815f9f1dc0aa70942d37
dsvipeer <112516086+dsvipeer@users.noreply.github.com>
2023-07-25 14:17:55 -0300

    Delete SpikeStripsCustom directory

--- SpikeStripsCustom/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'MajorFivePD on FiveM Forum: https://forum.cfx.re/u/majorfivepd/summary | dsvipeer on github: https://github.com/dsvipeer '
-description 'SpikeStripsCustom allows you to deploy (with sound) up to 2 strips that can cover good portion of roads and highways and will blow up tires of only NPCs.'
-version '1.0'
-
-client_script 'client.lua'
