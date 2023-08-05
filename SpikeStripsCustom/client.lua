873e9a24aef2934556f349cdf16ffb36b54450fd
dsvipeer <112516086+dsvipeer@users.noreply.github.com>
2023-08-05 16:42:09 -0300

    FIXED NOW ABLE TO CHANGE PEDS

--- SpikeStripsCustom/client.lua
+++ SpikeStripsCustom/client.lua
@@ -1,24 +1,8 @@
---MIT License
-
---Copyright (c) 2023 dsvipeer
-
---Permission is hereby granted, free of charge, to any person obtaining a copy
---of this software and associated documentation files (the "Software"), to deal
---in the Software without restriction, including without limitation the rights
---to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
---copies of the Software, and to permit persons to whom the Software is
---furnished to do so, subject to the following conditions:
-
---The above copyright notice and this permission notice shall be included in all
---copies or substantial portions of the Software.
-
---THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
---IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
---FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
---AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
---LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
---OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
---SOFTWARE. 
+--***************************************************************
+--*                                                           *--
+--*                SpikeStripsCustom by MajorFivePD          *--
+--*                                                           *--
+--***************************************************************
 
 
 local SpikeStrips = {}
@@ -59,17 +43,19 @@ end
 
 
 local function DeploySpikeStrips(spikesToSpawn)
-    if not PlayerPed then
-        return 
+    local playerPed = PlayerPedId() -- Fetch the player ped
+
+    if not playerPed then
+        return
     end
 
-    local playerPos = GetEntityCoords(PlayerPed)
-    local playerHeading = GetEntityHeading(PlayerPed)
+    local playerPos = GetEntityCoords(playerPed)
+    local playerHeading = GetEntityHeading(playerPed)
 
     PlayKneelAnim(spikesToSpawn * 2000)
 
     for i = 1, spikesToSpawn do
-        local forwardVector = GetEntityForwardVector(PlayerPed)
+        local forwardVector = GetEntityForwardVector(playerPed)
         local spawnOffset = forwardVector * (i * 3.7) + vector3(0.0, 0.0, -1.0)
         local spikeStrip = CreateSpikeStrip(playerPos + spawnOffset, playerHeading)
         table.insert(SpikeStrips, spikeStrip)
@@ -77,10 +63,8 @@ local function DeploySpikeStrips(spikesToSpawn)
 
     PlayDeploySound()
 
-    
     Citizen.Wait(500)
 
-   
     local function ShowNotification(text)
         SetNotificationTextEntry("STRING")
         AddTextComponentString(text)
